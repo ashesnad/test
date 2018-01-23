03becee05d5332af0d02c349f9313bcfe19e3454
Seungmin Hong <ashesnad@gmail.com>
2018-01-22 22:44:29 -0800

    hello.c modified 03

--- hello.c
+++ hello.c
@@ -5,5 +5,7 @@
 int main(){
   printf("Hello, World!\n");
   printf("sin(pi/2) = %.6f\n", sinf(M_PI/2.f));
+  printf("sin(pi/3) = %.6f\n", sinf(M_PI/3.f));
+  printf("sin(pi/4) = %.6f\n", sinf(M_PI/4.f));
   return 0;
 }
