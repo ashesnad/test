8e7aa6bda0409740119327386efce71cb1176764
Seungmin Hong <ashesnad@gmail.com>
2018-01-22 21:38:58 -0800

    hello.cc modified 01

--- hello.cc
+++ hello.cc
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main(){
   cout << "Hello, World!" << endl;
+  cout << "sin(pi/2) = " << sinf(M_PI/2.f) << endl;
   return 0;
 }
